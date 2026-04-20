9ae27d912f3c44d4b84f2fde8593eb2d99d037b5
Getha Varshini <varshinigetha@gmail.com>
2026-04-20 13:37:16 +0530

    UC1: Display empty Tic Tac Toe board

---
+++ UC1_TicTacToe.java
@@ -0,0 +1,27 @@
+public class UC1_TicTacToe {
+
+    static char[][] board = new char[3][3];
+
+    public static void main(String[] args) {
+        initializeBoard();
+        printBoard();
+    }
+
+    static void initializeBoard() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                board[row][col] = '-';
+            }
+        }
+    }
+
+    static void printBoard() {
+        System.out.println("UC1: Empty Board");
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
